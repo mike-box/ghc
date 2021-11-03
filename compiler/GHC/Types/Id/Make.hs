83231cec727f798da2133ebcc10c6a7d38a52b27
Adam Gundry <adam@well-typed.com>
2021-11-03 10:42:41 +0000

    Change Reduction to store original type and DCoercion

--- compiler/GHC/Types/Id/Make.hs
+++ compiler/GHC/Types/Id/Make.hs
@@ -1023,7 +1023,7 @@ dataConSrcToImplBang bang_opts fam_envs arg_ty
       srcUnpack -> isSrcUnpacked srcUnpack
   = case mb_co of
       Nothing   -> HsUnpack Nothing
-      Just redn -> HsUnpack (Just $ reductionCoercion redn)
+      Just redn -> HsUnpack (Just $ reductionCoercion Representational redn)
 
   | otherwise -- Record the strict-but-no-unpack decision
   = HsStrict
